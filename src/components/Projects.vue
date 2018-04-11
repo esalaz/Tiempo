d1e99892923bcf75cd9e4138745412cf774d321a
esalaz415 <esalaz415@gmail.com>
2018-04-11 13:55:33 -0700

    added axios to front-end

---
+++ src/components/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="projects">
+    This file will list all the posts.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+  data () {
+    return {}
+  }
+}
+</script>
