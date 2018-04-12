ab76d3453337c9f8c48a1df75989751cb7f032b6
esalaz415 <esalaz415@gmail.com>
2018-04-11 18:19:18 -0700

    added post vue

---
+++ src/components/Posts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    This file will list all the posts.
+
+    <div v-for="post in posts">
+      <p>
+        <span><b>{{ posts.title }}</b></span><br />
+        <span>{{ posts.description }}</span>
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import TiempoService from '@/Services/TiempoService'
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await TiempoService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
