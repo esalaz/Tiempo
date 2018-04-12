88cdcfd8848f93bddfba2cad41590b88527b1e36
esalaz415 <esalaz415@gmail.com>
2018-04-12 13:48:37 -0700

    connected mogodb in app.js to localhost:27017

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
 const app = express()
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
